cee366c5dd005941afc1bfc17bc0079d780b547b
Ryan Montville <ryan.a.montville@gmail.com>
2026-07-20 15:23:53 -0500

    updating db version to force refresh

--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -1,7 +1,7 @@
 import type { Achievement } from "../models";
 
 const DB_NAME = 'pwa_stats_db';
-const DB_VERSION = 5;
+const DB_VERSION = 6;
 const SETTINGS_STORE = 'settings';
 const ACHIEVEMENTS_STORE = 'achievements';
 const CLAIMED_KEY = 'claimed_user';
